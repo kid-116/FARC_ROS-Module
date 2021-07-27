6df29362c38f479349516053bcdd24a6fddf297c
kid-116 <mehultodi116@gmail.com>
2021-07-27 14:43:58 +0530

    Implemented the first assignment

---
+++ ros_ws/src/Chatterbot/scripts/talker_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)
+    for i in range(100):
+        hello_str = "Hello world - " + str(i + 1)
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
